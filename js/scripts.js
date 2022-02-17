15ca87780b475d22a2b293ad338cefbe22235b38
sansay2k <96008206+sansay2k@users.noreply.github.com>
2022-02-17 14:47:46 +0200

    my site full v1

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+// Custom Scripts
+// Мобильное меню бургер
+function burgerMenu() {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+  const body = document.querySelector('body');
+  const linkContainer = document.querySelector('.menu'); 
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active');
+      burger.classList.add('active-burger');
+      body.classList.add('locked');
+    } else {
+      menu.classList.remove('active');
+      burger.classList.remove('active-burger');
+      body.classList.remove('locked');
+    }
+  });
+
+  linkContainer.addEventListener('click', (evt) => {
+    if (evt.target.classList.contains('menu__item-link')) {
+      menu.classList.remove('active');
+      burger.classList.remove('active-burger');
+      body.classList.remove('locked');
+    }
+  });
+
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active');
+      burger.classList.remove('active-burger');
+      body.classList.remove('locked');
+    }
+  });
+}
+burgerMenu();
+
