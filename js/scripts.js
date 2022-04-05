eb3de453310956d2271cfbf24c0516b777a99e29
sansay2k <96008206+sansay2k@users.noreply.github.com>
2022-04-05 17:16:49 +0200

    All new site!

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// Custom Scripts
+wow = new WOW(
+  {
+  boxClass:     'wow',      // default
+  animateClass: 'animated', // default
+  offset:       0,          // default
+  mobile:       true,       // default
+  live:         true        // default
+}
+);
+wow.init();
+
+$(document).ready(function () {
+  $('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
+  
+});
+
+// WOW
